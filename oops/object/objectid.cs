f37781f9388e5f68ab1075c902b1c5d98894b562
anonymous1901j <sky1901j@gmail.com>
2025-08-31 10:45:17 +0530

    objectid.cs

---
+++ oops/object/objectid.cs
@@ -0,0 +1,99 @@
+class product 
+{
+	private int pid;
+	private string pname;
+	private double price;
+	public void read()
+	{
+		pid=int.Parse(System.Console.ReadLine());
+		pname=System.Console.ReadLine();
+		price=double.Parse(System.Console.ReadLine());
+	}
+	public void print()
+	{
+		System.Console.WriteLine("{0} {1} {2}",pid,pname,price);
+	}
+}
+class employee 
+{
+	private int eid;
+	private string ename;
+	private double salary;
+	public void read()
+	{
+		eid=int.Parse(System.Console.ReadLine());
+		ename=System.Console.ReadLine();
+		salary=double.Parse(System.Console.ReadLine());
+	}
+	public void print()
+	{
+		System.Console.WriteLine("{0} {1} {2}",eid,ename,salary);
+	}
+}
+class program
+{
+	public static void Main()
+	{
+		Items item=new Items(5);
+		product p=new product();
+		employee e=new employee();
+		p.read();
+		e.read();
+		item.Add(p);
+		item.Add(e);
+		item.Add(100);
+		item.Add(22.22);
+		item.Add("ravi");
+		for(int i=0;i<5;i++)
+		{
+			object o;
+			o=item.get();
+			if(o is product)
+			{
+				
+				(o as product).print();
+			}
+			else if(o is employee)
+			{
+				(o as employee).print();
+			}
+			else
+			{
+				System.Console.WriteLine(o);
+			}
+		
+		}
+	}
+}
+class Items
+{
+	private object []p;
+	int i;
+	public Items(int size)
+	{
+		p=new object[size];
+	}
+	public void Add(object r)
+	{
+		p[i]=r;
+		i++;
+	}
+	public object get()
+	{
+		return p[--i];
+	}
+}
+/* this is the input
+  1
+shiva
+123456
+2
+jaanu
+123456
+this is the output
+ravi
+22.22
+100
+2 jaanu 123456
+1 shiva 123456
+*/
