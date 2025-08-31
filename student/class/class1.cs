74792ce7beca492488eede48a33ae74a7059f559
anonymous1901j <sky1901j@gmail.com>
2025-08-31 16:39:49 +0530

    class1.cs

---
+++ student/class/class1.cs
@@ -0,0 +1,31 @@
+class student 
+{
+	private int pid;
+	private string pname;
+	private double price;
+	public void read()
+	{
+		pid=int.Parse(System.Console.ReadLine());
+		pname=System.Console.ReadLine();
+		price=double.Parse(System.Console.ReadLine());
+	}
+	public void print()
+	{
+		System.Console.WriteLine(" {0} {1} {2}",pid,pname,price);
+	}
+}
+class program
+{
+	public static void Main()
+	{
+		student s=new student();
+		s.read();
+		s.print();
+	}
+}
+/* the output of this program is
+1
+shiva
+20000000
+1 shiva 20000000
+*/
